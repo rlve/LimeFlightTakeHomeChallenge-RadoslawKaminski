f74f8d00c060577b42664c57e44c8511b4785833
rlve <radioslav@gmail.com>
2024-05-09 13:26:41 +0100

    add tests for checkboxes

--- lib/contact.page.ts
+++ lib/contact.page.ts
@@ -1,5 +1,16 @@
 import { type Page, type Locator, expect } from '@playwright/test';
 
+type FormFields = 'firstName' | 'lastName' | 'email' | 'company' | 'message';
+
+type Checkboxes =
+  | 'platform'
+  | 'loadPlanning'
+  | 'mealPlanning'
+  | 'mobileApp'
+  | 'inventoryManagement'
+  | 'routeOptimization'
+  | 'communication';
+
 export class ContactPage {
   private readonly contactSalesButton: Locator;
   private readonly getStartedButton: Locator;
@@ -17,6 +28,9 @@ export class ContactPage {
   private readonly communicationCheckbox: Locator;
   private readonly getStartedSubmitButton: Locator;
   private readonly successPopUp: Locator;
+
+  private formFieldsLocatorsMap: { [key in FormFields]: Locator };
+  private checkboxesLocatorsMap: { [key in Checkboxes]: Locator };
 
   constructor(public readonly page: Page) {
     this.contactSalesButton = page.getByRole('button', {
@@ -58,7 +72,27 @@ export class ContactPage {
       .filter({ hasText: 'Get Started with LimeFlight!' })
       .getByRole('button');
     this.successPopUp = page.getByText("Thanks!We'll be right with");
+
+    this.formFieldsLocatorsMap = {
+      firstName: this.firstNameInput,
+      lastName: this.lastNameInput,
+      email: this.emailInput,
+      company: this.companyInput,
+      message: this.messageInput,
+    };
+
+    this.checkboxesLocatorsMap = {
+      platform: this.platformModule,
+      loadPlanning: this.loadPlanningModule,
+      mealPlanning: this.mealPlanningModule,
+      mobileApp: this.mobileAppModule,
+      inventoryManagement: this.inventoryManagementModule,
+      routeOptimization: this.routeOptimizationModule,
+      communication: this.communicationCheckbox,
+    };
   }
+
+  // Actions
 
   async goto() {
     await this.page.goto('https://test.limeflight.com/');
@@ -66,20 +100,6 @@ export class ContactPage {
 
   async openContactForm() {
     await this.contactSalesButton.click();
-  }
-
-  async fillForm(data: {
-    firstName?: string;
-    lastName?: string;
-    email?: string;
-    company?: string;
-    message?: string;
-  }) {
-    if (data.firstName) await this.firstNameInput.fill(data.firstName);
-    if (data.lastName) await this.lastNameInput.fill(data.lastName);
-    if (data.email) await this.emailInput.fill(data.email);
-    if (data.company) await this.companyInput.fill(data.company);
-    if (data.message) await this.messageInput.fill(data.message);
   }
 
   async submit() {
@@ -90,25 +110,49 @@ export class ContactPage {
     await expect(this.successPopUp).toBeVisible({ visible });
   }
 
-  async hasFocus(
-    element: 'firstName' | 'lastName' | 'email' | 'company' | 'message',
-  ) {
-    switch (element) {
-      case 'firstName':
-        await expect(this.firstNameInput).toBeFocused();
-        break;
-      case 'lastName':
-        await expect(this.lastNameInput).toBeFocused();
-        break;
-      case 'email':
-        await expect(this.emailInput).toBeFocused();
-        break;
-      case 'company':
-        await expect(this.companyInput).toBeFocused();
-        break;
-      case 'message':
-        await expect(this.messageInput).toBeFocused();
-        break;
+  // Form fields
+
+  async fillForm(data: { [key in FormFields]?: string }) {
+    if (data.firstName) await this.firstNameInput.fill(data.firstName);
+    if (data.lastName) await this.lastNameInput.fill(data.lastName);
+    if (data.email) await this.emailInput.fill(data.email);
+    if (data.company) await this.companyInput.fill(data.company);
+    if (data.message) await this.messageInput.fill(data.message);
+  }
+
+  async hasFocus(element: FormFields) {
+    await expect(this.formFieldsLocatorsMap[element]).toBeFocused();
+  }
+
+  // Checkboxes
+
+  async checkElement(element: Checkboxes) {
+    await this.checkboxesLocatorsMap[element].check();
+  }
+
+  async uncheckElement(element: Checkboxes) {
+    await this.checkboxesLocatorsMap[element].uncheck();
+  }
+
+  async isChecked(element: Checkboxes, checked = true) {
+    await expect(this.checkboxesLocatorsMap[element]).toBeChecked({ checked });
+  }
+
+  // Modules
+
+  async uncheckModules() {
+    const { communication, ...modulesMap } = this.checkboxesLocatorsMap;
+
+    for (const element of Object.values(modulesMap)) {
+      await element.uncheck();
+    }
+  }
+
+  async areModulesChecked(checked = true) {
+    const { communication, ...modulesMap } = this.checkboxesLocatorsMap;
+
+    for (const element of Object.values(modulesMap)) {
+      await expect(element).toBeChecked({ checked });
     }
   }
 }
--- tests/contact.spec.ts
+++ tests/contact.spec.ts
@@ -3,22 +3,66 @@ import { test } from '../lib/fixtures';
 import { faker } from '@faker-js/faker';
 
 test.describe('Contact form', () => {
-  test('can be submitted succesfully with correct data', async ({
-    contactPage,
-  }) => {
-    await contactPage.openContactForm();
+  test.describe('submission', () => {
+    test('can be submitted succesfully with correct data', async ({
+      contactPage,
+    }) => {
+      await contactPage.openContactForm();
 
-    await contactPage.fillForm({
-      firstName: faker.person.firstName(),
-      lastName: faker.person.lastName(),
-      email: faker.internet.email(),
-      company: faker.company.name(),
-      message: faker.lorem.paragraph(),
+      await contactPage.fillForm({
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
+        company: faker.company.name(),
+        message: faker.lorem.paragraph(),
+      });
+
+      await contactPage.submit();
+
+      await contactPage.isSuccessPopUpVisible();
     });
 
-    await contactPage.submit();
+    test('can be submitted succesfully with modules unchecked', async ({
+      contactPage,
+    }) => {
+      await contactPage.openContactForm();
 
-    await contactPage.isSuccessPopUpVisible();
+      await contactPage.fillForm({
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
+        company: faker.company.name(),
+        message: faker.lorem.paragraph(),
+      });
+
+      await contactPage.uncheckModules();
+
+      await contactPage.submit();
+
+      await contactPage.isSuccessPopUpVisible();
+    });
+
+    test('can be submitted succesfully with communication checkbox unchecked', async ({
+      contactPage,
+    }) => {
+      await contactPage.openContactForm();
+
+      await contactPage.fillForm({
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
+        company: faker.company.name(),
+        message: faker.lorem.paragraph(),
+      });
+
+      await contactPage.uncheckElement('communication');
+
+      await contactPage.submit();
+
+      await contactPage.isSuccessPopUpVisible();
+    });
+
+    // another button
   });
 
   test.describe('validation', () => {
@@ -117,6 +161,72 @@ test.describe('Contact form', () => {
 
       await contactPage.isSuccessPopUpVisible(false);
       await contactPage.hasFocus('message');
+    });
+  });
+
+  test.describe('modules', () => {
+    test('are checked in default', async ({ contactPage }) => {
+      await contactPage.openContactForm();
+
+      await contactPage.areModulesChecked();
+    });
+
+    test('can be unchecked', async ({ contactPage }) => {
+      await contactPage.openContactForm();
+
+      await contactPage.uncheckElement('platform');
+      await contactPage.uncheckElement('routeOptimization');
+
+      await contactPage.isChecked('platform', false);
+      await contactPage.isChecked('routeOptimization', false);
+
+      // Checked in default
+      await contactPage.isChecked('loadPlanning');
+      await contactPage.isChecked('mealPlanning');
+    });
+
+    test('can be unchecked and checked again', async ({ contactPage }) => {
+      await contactPage.openContactForm();
+
+      await contactPage.uncheckElement('loadPlanning');
+      await contactPage.uncheckElement('mealPlanning');
+
+      await contactPage.isChecked('loadPlanning', false);
+      await contactPage.isChecked('mealPlanning', false);
+
+      await contactPage.checkElement('loadPlanning');
+      await contactPage.checkElement('mealPlanning');
+
+      await contactPage.isChecked('loadPlanning');
+      await contactPage.isChecked('mealPlanning');
+    });
+  });
+
+  test.describe('communication checkbox', () => {
+    test('is checked in default', async ({ contactPage }) => {
+      await contactPage.openContactForm();
+
+      await contactPage.isChecked('communication');
+    });
+
+    test('can be unchecked', async ({ contactPage }) => {
+      await contactPage.openContactForm();
+
+      await contactPage.uncheckElement('communication');
+
+      await contactPage.isChecked('communication', false);
+    });
+
+    test('can be unchecked and checked again', async ({ contactPage }) => {
+      await contactPage.openContactForm();
+
+      await contactPage.uncheckElement('communication');
+
+      await contactPage.isChecked('communication', false);
+
+      await contactPage.checkElement('communication');
+
+      await contactPage.isChecked('communication');
     });
   });
 });
